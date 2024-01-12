bae21db3bb20e9cbaafe8ac70e20750904b0e9e1
Eli Holzman <eli770440@gmail.com>
2024-01-12 09:22:40 +0200

    added section for js projects

---
+++ script/main.js
@@ -0,0 +1,5 @@
+document.getElementById('hamburgerContainer').onclick = function () {
+    let box = document.getElementById('box');
+    box.innerHTML =
+        '<ul><li><a href="#conectsContainer">צור קשר</a></li><li><ahref="#projectsContainer">שירותים</ahref=></li><li><a href="#onMe">אודות</a></li><li><a href="#">ראשי</a></li><ul>';
+};
