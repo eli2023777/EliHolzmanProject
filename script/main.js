46e9e4cf0896de7719e37228cea9bf2e8d3dc565
Eli Holzman <eli770440@gmail.com>
2024-02-15 10:37:58 +0200

    styling mobile mood

--- script/main.js
+++ script/main.js
@@ -1,5 +1,11 @@
+const navBarMobileContainer = document.querySelector('.navBarMobileContainer');
+
 document.getElementById('hamburgerContainer').onclick = function () {
     let box = document.getElementById('box');
-    box.innerHTML =
-        '<ul><li><a href="#conectsContainer">צור קשר</a></li><li><ahref="#projectsContainer">שירותים</ahref=></li><li><a href="#onMe">אודות</a></li><li><a href="#">ראשי</a></li><ul>';
+    // navBarMobile.style.display = 'block';
+    box.innerHTML = `<ul id='mobileUl'>
+    <li><a href="#conectsContainer">צור קשר</a></li>
+                    <li><a href="#projectsContainer">שירותים</a></li>
+                    <li><a href="#onMe">אודות</a></li>
+                    <li><a href="#">ראשי</a></li><ul>`
 };
